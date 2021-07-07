0cd500d508558f9bdec24d04bcae6e2bbf729106
wghou <wghou09@bjtu.edu.cn>
2021-07-07 21:44:57 +0800

    ss

--- Sample/Example2/Example2.cpp
+++ Sample/Example2/Example2.cpp
@@ -47,14 +47,19 @@ static Scene* g_scene = nullptr;
 
 static std::map<std::string, MaterialInstance*> g_meshMaterialInstances;
 static SandboxParameters g_params;
+static SandboxParameters g_params2;
 static Config g_config;
 static bool g_shadowPlane = false;
 
 static OBJLoader m_loader;
+static OBJLoader m_loader2;
 static utils::Entity renderable;
+static utils::Entity renderable2;
 
 VertexBuffer* vb = nullptr;
 IndexBuffer* ib = nullptr;
+VertexBuffer* vb2 = nullptr;
+IndexBuffer* ib2 = nullptr;
 
 
 static void cleanup(Engine* engine, View*, Scene*) {
@@ -73,6 +78,19 @@ static void cleanup(Engine* engine, View*, Scene*) {
 	engine->destroy(g_params.light);
 	EntityManager& em = EntityManager::get();
 	em.destroy(g_params.light);
+
+	// g_params2
+	for (auto& i : g_params2.materialInstance) {
+		engine->destroy(i);
+	}
+
+	for (auto& i : g_params2.material) {
+		engine->destroy(i);
+	}
+
+	engine->destroy(g_params2.light);
+	EntityManager& em2 = EntityManager::get();
+	em2.destroy(g_params2.light);
 }
 
 static void setup(Engine* engine, View* view, Scene* scene) {
@@ -82,15 +100,13 @@ static void setup(Engine* engine, View* view, Scene* scene) {
 	//view->setPostProcessingEnabled(false);
 	//view->setDepthPrepass(filament::View::DepthPrepass::DISABLED);
 
-	m_loader.loadObj(g_config.objFilename, false);
+	mat4f objTrans = mat4f{ mat3f(0.5f), float3(0.0f, -0.5f, 0.0f) }*
+		mat4f::rotation(0, float3(1.0f, 0.0f, 0.0f))*
+		mat4f::rotation(0, float3(0.0f, 1.0f, 0.0f))*
+		mat4f::rotation(0, float3(0.0f, 0.0f, 1.0f));
 
-	for (int i = 0; i < 12; i++)
-	{
-		std::cout << "i: " << i << "  position: " << m_loader.getVertices()[i*3+0] << "  " << m_loader.getVertices()[i * 3 + 1]
-			<< "  " << m_loader.getVertices()[i * 3 + 2] << std::endl;
-		std::cout << "i: " << i << "  tangent: " << m_loader.getTangents()[i * 3 + 0] << "  " << m_loader.getTangents()[i * 3 + 1]
-			<< "  " << m_loader.getTangents()[i * 3 + 2] << "  " << m_loader.getTangents()[i * 3 + 3] << std::endl;
-	}
+	m_loader.loadObj("../assets/models/livers/liver_S1.obj", false);
+	g_params.color = { 0.25f, 0.08f, 0.1f };
 
 	//// create material and light
 	createInstances(g_params, *engine);
@@ -128,7 +144,7 @@ static void setup(Engine* engine, View* view, Scene* scene) {
 	// set transform
 	auto& tcm = engine->getTransformManager();
 	auto ei = tcm.getInstance(renderable);
-	tcm.setTransform(ei, mat4f{ mat3f(g_config.scale), float3(0.0f, 0.0f, 0.0f) } *
+	tcm.setTransform(ei, objTrans *
 		tcm.getWorldTransform(ei));
 
 	// set material
@@ -144,6 +160,64 @@ static void setup(Engine* engine, View* view, Scene* scene) {
 	// add light
 	scene->addEntity(g_params.light);
 
+
+	//////////////////////////////////////////////////////////////
+	// obj 2
+	g_params2.color = { 0.5f, 0.1f, 0.1f };
+	m_loader2.loadObj("../assets/models/livers/liver_S2.obj", false);
+
+	//// create material and light
+	createInstances(g_params2, *engine);
+
+	// vertex buffer
+	VertexBuffer* vb2 = VertexBuffer::Builder()
+		.vertexCount(m_loader2.getNumVertices())
+		.bufferCount(2)
+		.attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT3)
+		.attribute(VertexAttribute::TANGENTS, 1, VertexBuffer::AttributeType::FLOAT4)
+		.normalized(VertexAttribute::TANGENTS)
+		.build(*engine);
+	vb2->setBufferAt(*engine, 0,
+		VertexBuffer::BufferDescriptor(m_loader2.getVertices().data(), m_loader2.getVertices().size() * sizeof(float), nullptr));
+	vb2->setBufferAt(*engine, 1,
+		VertexBuffer::BufferDescriptor(m_loader2.getTangents().data(), m_loader2.getTangents().size() * sizeof(float), nullptr));
+
+
+	// index buffer
+	IndexBuffer* ib2 = IndexBuffer::Builder()
+		.indexCount(m_loader2.getFaces().size())
+		.bufferType(IndexBuffer::IndexType::USHORT)
+		.build(*engine);
+	ib2->setBuffer(*engine,
+		IndexBuffer::BufferDescriptor(m_loader2.getFaces().data(), m_loader2.getFaces().size() * sizeof(uint16_t), nullptr));
+
+	// create renderable entity
+	EntityManager::get().create(1, &renderable2);
+
+	RenderableManager::Builder(1)
+		.boundingBox({ {-1.f, -1.f, -1.f}, {1.f, 1.f, 1.f} })
+		.geometry(0, RenderableManager::PrimitiveType::TRIANGLES, vb2, ib2, 0, m_loader2.getFaces().size())
+		.build(*engine, renderable2);
+
+	// set transform
+	auto& tcm2 = engine->getTransformManager();
+	auto ei2 = tcm2.getInstance(renderable2);
+	tcm2.setTransform(ei2, objTrans *
+		tcm2.getWorldTransform(ei2));
+
+	// set material
+	g_params2.currentMaterialModel = MATERIAL_LIT;
+	// set parameters
+	MaterialInstance* materialInstance2 = updateInstances(g_params2, *engine);
+	auto& rcm2 = engine->getRenderableManager();
+	auto instance2 = rcm2.getInstance(renderable2);
+	rcm2.setCastShadows(instance2, g_params2.castShadows);
+	rcm2.setMaterialInstanceAt(instance2, 0, materialInstance2);
+	scene->addEntity(renderable2);
+
+	// add light
+	scene->addEntity(g_params2.light);
+
 	return;
 }
 
@@ -156,7 +230,7 @@ static void preRender(filament::Engine*, filament::View* view, filament::Scene*,
 int main(int argc, char *argv[]) {
 
 	g_config.iblDirectory = "../envs/pillars";
-	utils::Path filename = "../assets/models/sphere2.obj";
+	utils::Path filename = "../assets/models/livers/liver_L1.obj";
 	if (!filename.exists()) {
 		std::cerr << "file " << filename << " not found!" << std::endl;
 		return 1;
